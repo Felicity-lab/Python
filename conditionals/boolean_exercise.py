e413a2439500bcd3fcc2468747f432820813102f
Felicity-lab <ftahwa01@gmail.com>
2020-06-30 19:35:02 +0800

    updated exercise

--- conditionals/boolean_exercise.py
+++ conditionals/boolean_exercise.py
@@ -8,7 +8,7 @@ print("No threats detected")
 #Que2
 moths_in_house = True
 mitch_is_home = True
-print("Hoooman! Hel me get the moths!")
+print("Hoooman! Help me get the moths!")
 
 moths_in_house = False
 mitch_is_home = False
@@ -22,4 +22,44 @@ moths_in_house = False
 mitch_is_home = True
 print("Climb on Mitch")
 
+#Que3
+light_colour = "Red" 
+car_detected = False 
 
+print()
+
+light_colour = "Red" 
+car_detected = True 
+print("Flash!")
+
+light_colour = "Green" 
+car_detected = False 
+print()
+
+light_colour = "Green" 
+car_detected = True 
+print()
+
+light_colour = "Amber" 
+car_detected = False 
+print()
+
+light_colour = "Amber" 
+car_detected = True 
+print()
+
+#Que4
+height = int(input('How tall are you? '))
+print(height)
+
+if height <= 50 and height <= 119:
+    print('Sorry, not today :(')
+
+elif height >= 120 and height <= 191:
+    print('Hop on!')
+
+# elif height == 191:
+    # print('Hop on!')
+
+else:
+    print()
