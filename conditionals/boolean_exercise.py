9389b5d9ec71ad2197d9be6394cdd38bd2af23ea
Felicity-lab <ftahwa01@gmail.com>
2020-06-30 18:13:29 +0800

    Create boolean_exercise.py

---
+++ conditionals/boolean_exercise.py
@@ -0,0 +1,25 @@
+#Que1
+moths_in_house = True
+print("Get the moths!")
+
+moths_in_house = False
+print("No threats detected")
+
+#Que2
+moths_in_house = True
+mitch_is_home = True
+print("Hoooman! Hel me get the moths!")
+
+moths_in_house = False
+mitch_is_home = False
+print("No threats detected.")
+
+moths_in_house = True
+mitch_is_home = False
+print("Meoooooooow! Hissssssss!")
+
+moths_in_house = False
+mitch_is_home = True
+print("Climb on Mitch")
+
+
