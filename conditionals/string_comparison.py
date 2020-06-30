44757d59bae8d1f182adc642fa8cc8940d54748b
Felicity-lab <ftahwa01@gmail.com>
2020-06-30 08:37:52 +0800

    Python introduction

---
+++ conditionals/string_comparison.py
@@ -0,0 +1,24 @@
+# name = "Faro"
+name = input("What is your name? ")
+print(name)
+
+if "a" in name:
+        print("There is an 'a' in your name!")
+if len(name) > 6:
+        print("Gosh! You have a long name!!!!")
+
+if name == "Felicity":
+    print ("Wow! What a great name!")
+elif name == "Sophie":
+    print ("Wow! What a cool name!")
+elif name == "Faro":
+    print ("Wow! You are really cool like your name!")
+elif name == "Taffy":
+    print ("Wow! You are awesome like your name!")   
+
+else:
+    print(f"Hi, {name}")
+
+
+
+    
---
+++ strings_and_numbers.py
@@ -0,0 +1,9 @@
+a_string = "cats"
+a_number = 3
+print(a_string + str(a_number))
+print(a_string * a_number)
+
+# print("dogs" + 3)
+# print("dogs" + "3")
+# print("dogs" * 3)
+# print("dogs" * 3.0)
