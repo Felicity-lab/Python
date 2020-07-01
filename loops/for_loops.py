dabd67d783b0e2fc2fb778173348755388c59e2d
Felicity-lab <ftahwa01@gmail.com>
2020-07-01 19:54:21 +0800

    Update for_loops.py

--- loops/for_loops.py
+++ loops/for_loops.py
@@ -38,4 +38,15 @@ for tea_category in tea_collection:
     print(f"{tea_category[0]} Teas:")
     for tea in tea_category[1:]:
         print(f"    {tea}")
-        
+
+groceries = [
+    ["Baby Spinach", 2.78],
+    ["Hot Chocolate", 3.70],
+    ["Crackers", 2.10],
+    ["Bacon", 9.00],
+    ["Carrots", 0.56],
+    ["Oranges", 3.08]
+]
+
+for item in groceries:
+    print(f"{item[0]:<20} ${item[1]:.2f}")
