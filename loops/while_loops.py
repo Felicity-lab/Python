8b29b633c7741cac9506eebf32c79e22a4c8f12f
Felicity-lab <ftahwa01@gmail.com>
2020-07-01 20:01:31 +0800

    Create while_loops.py

---
+++ loops/while_loops.py
@@ -0,0 +1,9 @@
+counter = 10
+
+while counter > 0:
+    print(counter)
+    if  counter%2 == 0:
+        print(f"{counter} is an even number.")
+    else:
+        print(f"{counter} is an odd number.")
+    counter = counter - 1
